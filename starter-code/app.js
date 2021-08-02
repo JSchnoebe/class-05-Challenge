a5bf91b60ef6da73bd6b16523ac847188d3e5d10
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-08-02 10:01:05 -0500

    added 2 more functions

--- starter-code/app.js
+++ starter-code/app.js
@@ -79,12 +79,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    let theSum = sum(sumArr[0], sum(sumArr[1],sumArr[2])[0])[0];
+    let message = sumArr + ' was passed in as an array of numbers, and ' + theSum + ' is their sum.'
+
+    return[theSum, message];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -100,6 +104,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    let product = multiply(multArr[0], multiply(multArr[1],multArr[2])[0])[0];
+    let message = 'The numbers ' + multArr + ' have a product of ' + product + '.';
+    return [product,message];
 
 }
 
